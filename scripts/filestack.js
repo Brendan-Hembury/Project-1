6d407663cec2878117e3bb04d6bbd2391459217e
Julie Theriault <joules2319@gmail.com>
2020-01-08 18:36:36 -0700

    added button functionality

--- scripts/filestack.js
+++ scripts/filestack.js
@@ -1,9 +1,8 @@
 var apiKey = "AzOa2BjoCRqov0LZHBNJNz";
 var imageURL = "";
-$("#original-image").on("click", "img", function () {
 
-	imageURL = $(this).attr("src");
 
+function display() {
 	var transformURL = "https://process.filestackapi.com/" + apiKey + "/ascii=colored:true,background:black,size:50,reverse:true/" + imageURL;
 	$.ajax({
 		url: transformURL,
@@ -12,20 +11,17 @@ $("#original-image").on("click", "img", function () {
 		.then(function (image) {
 			var imgString = JSON.stringify(image);
 			var imgDisp = imgString.replace(/\"|\\|<html>|<\/html>|<body>|<\/body>|<head>|<\/head>|<body style="background: #000000">|<body style=\"background: #000000\">/ig, "");
-			$("#text-image").attr("class", "grey darken-4")
+			$("#text-image").attr("class", "grey darken-4");
 			$("#text-image").html(imgDisp);
-			// var results = response.data;
 
-			// for (var i = 0; i < results.length; i++) {
-			// 	var textImgDiv = $("<div>");
-
-			// 	var image = $("<img>");
-			// 	image.attr("src", results[i].images.original_still.url);
-			// 	image.attr("alt", "image of " + search);
-			// 	textImgDiv.prepend(image);
-
-			// 	$("#text-image").prepend(textImgDiv);
-			// }
 		});
+};
+$("#original-image").on("click", "img", function () {
+	imageURL = $(this).attr("data-image");
+	display();
 });
 
+$("#original-image").on("click", "button", function () {
+	imageURL = $(this).attr("data-image");
+	display();
+});
