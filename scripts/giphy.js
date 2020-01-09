016f988ef88861fea5337705861b0f08ba424eed
Julie Theriault <joules2319@gmail.com>
2020-01-08 20:17:18 -0700

    removed button and card class from images

--- scripts/giphy.js
+++ scripts/giphy.js
@@ -2,7 +2,7 @@ $("form").on("submit", function () {
 	event.preventDefault();
 	var search = $("#search").val();
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-		search + "&api_key=0MjmS6h9ogtPWtK6B3SV3vVy1PfZbR9y&limit=10";
+		search + "&api_key=0MjmS6h9ogtPWtK6B3SV3vVy1PfZbR9y&limit=12";
 
 	$.ajax({
 		url: queryURL,
@@ -18,20 +18,20 @@ $("form").on("submit", function () {
 				var image = $("<img>");
 				image.attr("src", results[i].images.fixed_width_still.url);
 				image.attr("alt", "image of " + search);
-				image.attr("class", "hoverable")
+				image.attr("class", "hoverable responsive-img")
 				image.attr("data-image", results[i].images.fixed_width_still.url);
-				var convert = $("<button>");
-				convert.attr("class", "btn transparent left");
-				convert.attr("data-image", results[i].images.fixed_width_still.url);
-				convert.html("<mark class='grey lighten-3'>Convert to Art</mark>");
-				var span = $("<span>").attr("class", "card-title left");
-				span.append(convert);
+				// var convert = $("<button>");
+				// convert.attr("class", "btn transparent left");
+				// convert.attr("data-image", results[i].images.fixed_width_still.url);
+				// convert.html("<mark class='grey lighten-3'>Convert to Art</mark>");
+				// var span = $("<span>").attr("class", "card-title left");
+				// span.append(convert);
 
 				var cardImg = $("<div>");
 				cardImg.attr("class", "card-image");
-				cardImg.append(image, span);
+				cardImg.append(image);
 				var card = $("<div>");
-				card.attr("class", "card grey darken-3 image-height");
+				card.attr("class", "grey darken-3 image-height");
 				card.prepend(cardImg);
 				originalImgDiv.prepend(card);
 
